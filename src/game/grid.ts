6807a3488e054a9cc6e410e3579cb66b65b94d91
ADBond <48208438+ADBond@users.noreply.github.com>
2026-05-22 22:18:15 +0100

    better grid data structure

--- src/game/grid.ts
+++ src/game/grid.ts
@@ -1,22 +1,39 @@
 import { Card, getFullPack } from "./card";
 import { Player } from "./player";
 
+type GridEntry = {
+    card: Card,
+    player: Player | null,
+    faceup: boolean | null,
+}
+
 export class Grid {
-    private cards: [Card, Player | null, boolean | null][];
+    private cards: {[key: string]: GridEntry};
 
     constructor() {
         const pack = getFullPack();
-        this.cards = pack.map(card => [card, null, null]);
+        this.cards = Object.fromEntries(
+            pack.map(
+                card => [
+                    card.toStringShort(),
+                    {"card": card, "player": null, "faceup": null}
+                ]
+            )
+        );
     }
 
     play(card: Card, player: Player): void {
-        const cardArr = this.cards.filter(
-            ([c, _p, _b]) => Card.cardEquals(c, card)
-        )[0];
-        if (cardArr[1] !== null) {
+        const cardStr = card.toStringShort();
+        if (this.cards[cardStr].player !== null) {
             throw Error(`Card already played! ${card}`)
         }
-        cardArr[1] = player;
-        cardArr[2] = true;  // face up at the point of playing
+
+        this.cards[cardStr].player === player;
+        this.cards[cardStr].faceup === true;
+    }
+
+    turndown(card: Card): void {
+        const cardStr = card.toStringShort();
+        this.cards[cardStr].faceup === false;
     }
 }
